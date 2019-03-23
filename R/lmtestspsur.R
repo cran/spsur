61716dd64746be08ea9fbc5da8e35a04bde82ee4
Roman Minguez <roman.minguez@uclm.es>
2019-03-23 09:10:02 +0000

    version 1.0.0.3

--- R/lmtestspsur.R
+++ R/lmtestspsur.R
@@ -113,7 +113,6 @@
 #' LMs <- lmtestspsur(Form = Tformula, data = spc, W = Wspc)
 #'
 #' \dontrun{
-#' ## Not run:
 #' #################################################
 #' ######## PANEL DATA (G>1; Tm>1)          ########
 #' #################################################
@@ -130,6 +129,23 @@
 #'             PS90 + UE90 + RD90 + PO90
 #' LMs <- lmtestspsur(Form = Tformula, data = NCOVR, W = W)
 #' }
+#'
+#' ################################################################
+#' ######## PANEL DATA: TEMPORAL CORRELATIONS (G=1; Tm>1) ########
+#' ################################################################
+#' \donttest{
+#' #### Example 3: NCOVR in panel data form
+#' ## Takes less than 1 minute
+#' data("NCOVR")
+#' Year <- as.numeric(kronecker(c(1960,1970,1980,1990),matrix(1,nrow = dim(NCOVR)[1])))
+#' HR <- c(NCOVR$HR60,NCOVR$HR70,NCOVR$HR80,NCOVR$HR90)
+#' PS <- c(NCOVR$PS60,NCOVR$PS70,NCOVR$PS80,NCOVR$PS90)
+#' UE <- c(NCOVR$UE60,NCOVR$UE70,NCOVR$UE80,NCOVR$UE90)
+#' NCOVRpanel <- as.data.frame(cbind(Year,HR,PS,UE))
+#' Tformula <- HR ~ PS + UE
+#' LM_time <- lmtestspsur(Form = Tformula, data = NCOVRpanel, time = Year, W = W)
+#' }
+#'
 #' @export
 #'
 lmtestspsur <- function(Form = NULL, data = NULL, W = NULL,
--- R/spsurtime.R
+++ R/spsurtime.R
@@ -127,6 +127,60 @@
 #' pspc_slm <- spsurtime(Form = form_pspc, data = pspc, W = Wspc,
 #'                       time = pspc$index_time, type = "slm", method = "3sls")
 #'                       summary(pspc_slm)
+#' \donttest{
+#' ## Example 2:
+#' rm(list = ls()) # Clean memory
+#' data(NCOVR,package="spsur")
+#' N <- nrow(NCOVR)
+#' Tm <- 4
+#' index_time <- rep(1:Tm, each = N)
+#' index_indiv <- rep(1:N, Tm)
+#' pHR <- c(NCOVR$HR60, NCOVR$HR70, NCOVR$HR80, NCOVR$HR90)
+#' pPS <- c(NCOVR$PS60, NCOVR$PS70, NCOVR$PS80, NCOVR$PS90)
+#' pUE <- c(NCOVR$UE60, NCOVR$UE70, NCOVR$UE80, NCOVR$UE90)
+#' pNCOVR <- data.frame(indiv = index_indiv, time = index_time,
+#'                      HR = pHR, PS = pPS, UE = pUE)
+#' rm(NCOVR)
+#' form_pHR <- HR ~ PS + UE
+#'
+#' # SIM by ML
+#'
+#' ## pHR_sim <- spsurtime(Form = form_pHR, data = pNCOVR, W = W,
+#' ##                    time = pNCOVR$time, type = "sim", method = "ml")
+#' ## summary(pHR_sim)
+#'
+#' # SLM by 3SLS
+#'
+#' pHR_slm <- spsurtime(Form = form_pHR, data = pNCOVR, W = W,
+#'                      time = pNCOVR$time, type = "slm", method = "3sls")
+#' summary(pHR_slm)
+#'
+#' ############################ LR tests about betas in spatio-temporal models
+#'
+#' ## Usually takes less than 1 minute
+#' ## H0: equal PS and UE beta in equations 3 and 4 al
+#' R <- matrix(0,nrow=2,ncol=12)
+#' R[1,8] <- 1; R[1,11] <- -1
+#' R[2,9] <- 1; R[2,12] <- -1
+#' b <- matrix(0, nrow = 2, ncol = 1)
+#' lr_partrate <-  lr_betas_spsur(Form = form_pHR, data = pNCOVR,
+#'                                time = pNCOVR$time, W = W,
+#'                                type = "sim", R = R, b = b, trace = TRUE,
+#'                                printmodels = TRUE)
+#'
+#' ############################ Wald tests about betas in spatio-temporal models
+#' wald_betas(pHR_sim , R = R , b = b) # SIM model
+#' wald_betas(pHR_slm , R = R , b = b) # SLM model
+
+#' ############################ Wald tests about spatial-parameters in
+#' ############################ spatio-temporal models
+#' ## H0: equal lambdas in slm model for equations 1 and 2.
+#' R2 <- matrix(0, nrow = 1, ncol = 4)
+#' R2[1,1] <- 1; R2[1,2] <- -1
+#' b2 <- matrix(0, nrow = 1, ncol = 1)
+#' wald_deltas(pHR_slm, R = R2, b = b2)
+#' }
+#'
 #' @export
 spsurtime <- function(Form, data, time, type = "sim",  method = "ml",
                       maxlagW = 2, W = NULL, cov = TRUE,
